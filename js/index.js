8c3c4f428e43c3e8434b7478d6edd22ce417d484
Sebastian <darkopx56@gmail.com>
2024-04-18 20:18:03 -0300

    0.0.1

---
+++ js/index.js
@@ -0,0 +1,22 @@
+// //tipo de dato string
+// let nombre = "mara"; 
+// //Tipo de dato Numeros
+// let numero = 30;
+// //tipo de dato booleano
+// let bool = true;
+// let falso = false;
+// //tipo de dato undefined
+// let indefinido;
+// //tipo de dato undefined
+// let nulo = null;
+// //constantes "const" son valores predefinidos inreasignables
+// //console.log permite mostrar en la consola una variable escrita y definida
+
+// let apellido = prompt("ingrese su nombre");
+// console.log(apellido);
+
+let nombreProducto = "heladera"
+let precioUnitario = 10
+let cantidadDeseada = prompt("Ingrese la Cantidad de heladeras que quiera")
+let costoFinal = precioUnitario * cantidadDeseada
+alert(costoFinal)
